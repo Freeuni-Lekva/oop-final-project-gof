24c43bde6a06b5cef7bad6140bc130caefa06a35
Giorgi Gelashvili <ggela22@freeuni.edu.ge>
2025-06-06 18:24:47 +0400

    Add Story class, to be implemented

---
+++ story-ai/src/main/java/model/story/Story.java
@@ -0,0 +1,12 @@
+package model.story;
+
+// TODO: Implement Story class
+public class Story {
+    public Story(String title, String prompt, int userId) {
+
+    }
+
+    public Story getStoryId() {
+        return null;
+    }
+}
