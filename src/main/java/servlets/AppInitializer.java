0e7ee05ddda0ad4c5a5ba97694e9a6ab16e80769
chichi-a <kchic22@freeuni.edu.ge>
2025-06-11 15:10:11 +0400

    Create shared Dao instances

--- src/main/java/servlets/AppInitializer.java
+++ src/main/java/servlets/AppInitializer.java
@@ -1,4 +1,9 @@
 package servlets;
+import data.*;
+import data.chat.*;
+import data.media.*;
+import data.story.*;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
@@ -6,7 +11,19 @@ public class AppInitializer implements ServletContextListener, HttpSessionListen
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        ServletContext context = sce.getServletContext();
+        MySqlConnector mySqlConnector = new MySqlConnector();
 
+        context.setAttribute("userDao", new UserDAO(mySqlConnector));
+        context.setAttribute("storyDao", new StoryDAO(mySqlConnector));
+        context.setAttribute("chatDao", new ChatDAO(mySqlConnector));
+        context.setAttribute("messageDao", new MessageDAO(mySqlConnector));
+        context.setAttribute("postDao", new PostDAO(mySqlConnector));
+        context.setAttribute("commentDao", new CommentsDAO(mySqlConnector));
+        context.setAttribute("likeDao", new LikesDAO(mySqlConnector));
+        context.setAttribute("tagDao", new TagsDAO(mySqlConnector));
+
+        context.setAttribute("mySqlConnector", mySqlConnector);
     }
 
     @Override
