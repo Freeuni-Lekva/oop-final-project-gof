9ea5a258a1c75bab008dcae6edd72542a5d74250
chichi-a <kchic22@freeuni.edu.ge>
2025-06-13 13:38:44 +0400

    Filled messageDao methods

--- src/main/java/data/chat/MessageDAO.java
+++ src/main/java/data/chat/MessageDAO.java
@@ -1,8 +1,9 @@
 package data.chat;
 
 import data.MySqlConnector;
-import model.Message;
+import model.chat.Message;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 /**
@@ -11,16 +12,60 @@ import java.util.ArrayList;
 public class MessageDAO {
     private MySqlConnector connector;
 
-    public MessageDAO(MySqlConnector sqlConnector) {
-        connector = sqlConnector;
+    public MessageDAO(MySqlConnector connector) {
+        this.connector = connector;
     }
 
-    public String getMessage(int message_id){
+    public String getMessage(int message_id) throws SQLException {
+        String sql = "SELECT message FROM messages WHERE message_id = ?";
+
+        try (Connection conn = connector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, message_id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return rs.getString("message");
+            }
+        }
+
         return "";
     }
 
-    public ArrayList<Message> getMessages(int chat_id){
-        return null;
+    public ArrayList<Message> getMessages(int chat_id) throws SQLException {
+        String sql = "SELECT message_id, message, is_user FROM messages WHERE chat_id = ? ORDER BY message_id ASC";
+        ArrayList<Message> messages = new ArrayList<>();
+
+        try (Connection conn = connector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, chat_id);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                String msg = rs.getString("message");
+                boolean isUser = rs.getBoolean("is_user");
+                int id = rs.getInt("message_id");
+                messages.add(new Message(msg, isUser, id));
+            }
+        }
+
+        return messages;
+    }
+
+    public int addMessage(int chat_id, String msg, boolean isUser) throws SQLException {
+        String sql = "INSERT INTO messages (message, chat_id, is_user) VALUES (?, ?, ?)";
+
+        try (Connection conn = connector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, msg);
+            stmt.setInt(2, chat_id);
+            stmt.setBoolean(3, isUser);
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        }
+
+        return -1;
     }
 
 }
