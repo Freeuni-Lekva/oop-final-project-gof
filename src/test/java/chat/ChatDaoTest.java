c361a944796cdc385f8de69ab58bbd7973b41ab1
chichi-a <kchic22@freeuni.edu.ge>
2025-06-14 14:11:03 +0400

    create test class for chats

---
+++ src/test/java/chat/ChatDaoTest.java
@@ -0,0 +1,31 @@
+package chat;
+
+import data.MySqlConnector;
+import junit.framework.TestCase;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ChatDaoTest extends TestCase {
+
+    private Connection conn;
+
+    @Override
+    public void setUp() throws SQLException {
+        conn = MySqlConnector.getConnection();
+    }
+
+
+    public void testConnectionNotNull() {
+        assertNotNull("Connection should not be null", conn);
+    }
+
+
+
+    @Override
+    public void tearDown() {
+        MySqlConnector.close(conn);
+        assertTrue("Connection should be closed after test", true);
+    }
+
+}
