ccb50e99ad22ed2360e5d6ff77cab6633857b38a
chichi-a <kchic22@freeuni.edu.ge>
2025-06-23 17:47:29 +0400

    refactored post servlet

--- src/main/java/servlets/PostServlet.java
+++ src/main/java/servlets/PostServlet.java
@@ -2,18 +2,14 @@ package servlets;
 
 import data.story.StoryDAO;
 import data.user.UserDAO;
-import jakarta.servlet.HttpConstraintElement;
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import model.User;
 import model.story.Story;
-import org.mindrot.jbcrypt.BCrypt;
 
 import java.io.IOException;
 import java.sql.SQLException;
