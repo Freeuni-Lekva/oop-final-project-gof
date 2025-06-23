841c38a02094d7479e3a3fc4f9e21e65b06d440b
chichi-a <kchic22@freeuni.edu.ge>
2025-06-23 16:10:06 +0400

    basic button logic for post servlet

--- src/main/java/servlets/PostServlet.java
+++ src/main/java/servlets/PostServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import data.story.StoryDAO;
 import data.user.UserDAO;
 import jakarta.servlet.HttpConstraintElement;
 import jakarta.servlet.RequestDispatcher;
@@ -11,6 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import model.User;
+import model.story.Story;
 import org.mindrot.jbcrypt.BCrypt;
 
 import java.io.IOException;
@@ -20,12 +22,53 @@ import java.sql.SQLException;
 public class PostServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        String storyIdStr = req.getParameter("storyId");
+        String username = (String) req.getSession().getAttribute("user");
 
+        ServletContext context = getServletContext();
+        StoryDAO storyDAO = (StoryDAO) context.getAttribute("storyDao");
+        UserDAO userDAO = (UserDAO) context.getAttribute("userDao");
+
+        if (username == null) {
+            res.sendRedirect(req.getContextPath() + "/login.jsp");
+            return;
+        }
+        if (action == null || storyIdStr == null) {
+            res.sendRedirect(req.getContextPath() + "/home");
+            return;
+        }
+
+
+        try {
+            User user = userDAO.findUser(username);
+            int userId = user.getUserId();
+            int storyId = Integer.parseInt(storyIdStr);
+            Story story = storyDAO.getStory(storyId);
+
+            switch (action) {
+                case "bookmark":
+
+                    userDAO.addBookmark(userId,story);
+                    res.sendRedirect(req.getContextPath() + "/post.jsp?id=" + storyId);
+                    break;
+
+                case "start_story":
+                    res.sendRedirect(req.getContextPath() + "/AIchat.jsp?storyId=" + storyId);
+                    break;
+
+                default:
+                    res.sendRedirect(req.getContextPath() + "/home");
+                    break;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
